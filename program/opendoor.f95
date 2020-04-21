d4ad1bf6f3a3e3bf1e5be258bae8333e8e0b5e25
seanpcallahan <46636572+seanpcallahan@users.noreply.github.com>
2020-04-20 22:08:51 -0700

    Update opendoor.f95
    so far this is a simple door code checker

--- program/opendoor.f95
+++ program/opendoor.f95
@@ -1,1 +1,27 @@
-!hello
+ function check_door_code(code)
+    integer, intent(in) :: code
+    logical :: check_door_code
+    integer :: solution
+    solution = 638
+    check_door_code = code==solution
+    return
+ end function
+    
+ program try_door
+    implicit none
+    logical, external :: check_door_code
+    integer :: upper
+    integer :: i
+    logical :: code_found
+    upper = 1000
+    DO i=1, upper
+        IF (check_door_code(i)) THEN
+            print*, "code is ", i
+            exit
+        END IF
+    END DO
+    
+    IF (i > 999) THEN
+        print*, "no code found"
+    END IF
+ end program
